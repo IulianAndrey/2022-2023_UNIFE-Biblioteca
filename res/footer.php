4088799c3cb75796ff88b40f5977835edff2dc31
AnDrEy <hiulianandrei@gmail.com>
2024-01-22 12:48:45 +0100

    Site resources

---
+++ res/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>progetto Basi Dati 2022-2023 realizzato da Giorgia Pirelli e Iulian Andrei Halapet</p>
+    <div style="text-align: center;">
+        <img src="/res/logo_unife_footer.png" width="100px">
+    </div>
+    <!--li attributi "width" [larghezza] ed "height" [altezza] specificano le dimensioni di un'immagine in pixel. -->
+</footer>
---
+++ res/head.php
@@ -0,0 +1,12 @@
+<?php 
+$parentDir = dirname(__DIR__);
+require_once($parentDir . '/PHP/connection_database.php');
+?>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--<link rel="stylesheet" href="/res/css/bootstrap.min.css" />-->
+    <link rel="stylesheet" href="/res/style.css" />
+    <link rel="shortcut icon" href="/res/logo_unife.png" type="image/png" />
+    <title><?php echo $pageTile ?></title>
+</head>

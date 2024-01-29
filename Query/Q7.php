672e8433166fbe18f5330b4a9a444b89e6936e32
AnDrEy <hiulianandrei@gmail.com>
2024-01-29 16:28:24 +0100

    Query 7 working

---
+++ Query/Q7.php
@@ -0,0 +1,62 @@
+<?php
+$parentDir = dirname(__DIR__);
+require_once($parentDir . '/PHP/connection_database.php');
+require_once($parentDir . '/PHP/funcBuildTable.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$pageTile = "Query7";
+include($parentDir . '/res/head.php');
+?>
+<body>
+    <div class="container">
+        <h1> Inserisci il range di date </h1>
+
+        <form method="POST" action="Q7.php">
+            <br><input type="date" name="data1" /></br>
+
+            <br><input type="date" name="data2" /></br>
+            <br><input type="submit" class="submit" value="Ricerca" /></br>
+            <hr />
+        </form>
+    </div>
+    <?php
+    $parametroRicerca = $_POST['data1'];
+    $parametroRicerca2 = $_POST['data2']; /*inserendo il range di date nel parametro_ricerca*/
+
+
+    if (!empty($parametroRicerca) && !empty($parametroRicerca2)) {
+        echo $prima_data;
+        /*sql query 7*/
+        $query = "SELECT *
+            FROM Prestito
+            WHERE data_rilascio BETWEEN '$parametroRicerca' AND '$parametroRicerca2'"
+        ;
+
+        $result = $conn->query($query);
+
+
+        /*chiamiamo funzione che costruisce tabella*/
+        echo BuildTable($result);
+    } else {
+        $giorni = 30;
+        echo 'LIBRI IN SCADENZA';
+        $query = "SELECT Utente.nome_u,Utente.cognome_u,Utente.telefono_u, Prestito.data_rilascio,Libro.titolo, Dipartimento.nome_d
+            FROM Prestito,Utente,Libro, Dipartimento
+            WHERE Utente.matricola=Prestito.matricola
+            AND Libro.id_libro=Prestito.id_libro
+            AND Libro.id_dipartimento=Dipartimento.id_dipartimento
+            AND restituito=0 AND data_rilascio < date_sub(current_date(), interval '$giorni' day)
+        order by data_rilascio;"
+        ;
+        $result = $conn->query($query);
+        echo BuildTable($result);
+    }
+    ?>
+
+    <?php include $parentDir . '/res/footer.php'; ?>
+</body>
+</html>
